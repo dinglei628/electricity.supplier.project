fbac6f302b347d060e191649f34c8a2c2ba70c24
“dinglei <l>
2020-03-02 15:55:22 +0800

    project

--- electricity.supplier.project/my.project.gateway/src/main/java/com/zb/MyGatWayServerApp.java
+++ electricity.supplier.project/my.project.gateway/src/main/java/com/zb/MyGatWayServerApp.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 public class MyGatWayServerApp {
+    //你好李四
     public static void main(String[] args) {
         SpringApplication.run(MyGatWayServerApp.class, args);
     }
