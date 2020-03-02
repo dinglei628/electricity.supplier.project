49df9df84623c1282ac16f652ff5a4130f90c683
dinglei628 <57700409+dinglei628@users.noreply.github.com>
2020-03-02 16:03:26 +0800

    Update MyGatWayServerApp.java

--- electricity.supplier.project/my.project.gateway/src/main/java/com/zb/MyGatWayServerApp.java
+++ electricity.supplier.project/my.project.gateway/src/main/java/com/zb/MyGatWayServerApp.java
@@ -7,10 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 public class MyGatWayServerApp {
-    //你好李四
-    //你好周勇
-    //你好何秋毫
-    //你好胡宗恩
     public static void main(String[] args) {
         SpringApplication.run(MyGatWayServerApp.class, args);
     }
