e7e1d13993f76db39168f3edd35f7e8f628485ee
root <528120788@qq.com>
2020-03-26 12:42:11 +0800

    2020-03-26 ADD PUSHMGS

--- electricity.supplier.project/my.project.order.api/src/main/java/com/demo/service/OrderService.java
+++ electricity.supplier.project/my.project.order.api/src/main/java/com/demo/service/OrderService.java
@@ -18,14 +18,20 @@ public interface OrderService {
     String ERROR_USER_NOT = "10001";
     //商品不存在
     String ERROR_GOOD_NOT = "10002";
-
+    //订单出错
     String ERROR_ORDER_NOT = "10003";
+    //订单重复
     String ERROR_ORDER_REPEAT = "10004";
+    //订单不存在
     String ERROR_ORDER_NON_EXISTENT = "10006";
 
     @PostMapping("/createOrder")
     Dto createOrder(@RequestParam(value = "token") String token,
                     @RequestParam(value = "goodsId") String goodsId);
+
+
+    @GetMapping("/getOrderById/{id}")
+    Dto getOrderById(@PathVariable("id")String id);
 
     @GetMapping("/searchOrder/{token}")
     Dto searchOrder(@PathVariable("token")String token,
